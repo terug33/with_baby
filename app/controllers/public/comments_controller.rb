a4e9e72468291b0de39f4f932068a81e1fa22649
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-07-19 15:27:15 +0000

    public投稿詳細

---
+++ app/controllers/public/comments_controller.rb
@@ -0,0 +1,31 @@
+class Public::CommentsController < Public::BaseController
+
+  def create 
+    @store = Store.find(params[:store_id])
+    @comment = @store.comments.build(comment_params)
+    @comment.user = current_user
+    if @comment.save 
+      redirect_to store_path(@store), notice: 'コメントを投稿しました'
+    else 
+      render 'stores/show'
+    end 
+  end 
+
+  def destroy
+    @comment = Comment.find(params[:id])
+    @store = @comment.store 
+    if @comment.user == current_user
+      @comment.destroy
+      redirect_to store_path(@store), notice: 'コメントを削除しました'
+    else
+      redirect_to store_params(@store), alert: '削除権限がありません'
+    end 
+  end 
+
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end 
+end
