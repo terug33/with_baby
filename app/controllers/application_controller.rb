126b3ec72251179c58c718805d3f599de0ddd215
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-07-20 10:03:30 +0000

    ログイン後のアクセス先設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,7 @@ class ApplicationController < ActionController::Base
 
 
   protected
+
   def configure_pemitted_parameters
 
     #新規登録時にnameを許可
@@ -12,4 +13,13 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end 
 
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(Admin)
+      admin_root_path
+    elsif resource.is_a?(User)
+      mypage_path
+    else 
+      root_path 
+    end 
+  end 
 end
