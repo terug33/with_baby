8348885db6931a89803e1b3155f7121b110cc10b
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-07-20 09:24:23 +0000

    マイページ

---
+++ app/controllers/public/users_controller.rb
@@ -0,0 +1,38 @@
+class Public::UsersController < Public::BaseController
+  before_action :authenticate_user!
+
+  def show 
+    @user = current_user
+    @stores = @user.stores.order(created_at: :desc)  #マイページでの投稿一覧用
+  end 
+
+  def search
+  end
+
+  def edit
+    @user = current_user
+  end
+
+  def update
+    @user = current_user
+    if @user.update(user_params)
+      redirect_to mypage_path, notice: 'プロフィールを更新しました'
+    else 
+      render :edit
+    end 
+  end 
+
+  def destroy
+    current_user.destroy
+    reset_session
+    redirect_to new_user_registration_path, notice: '退会しました'
+  end 
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name,:email)
+  end 
+
+end
