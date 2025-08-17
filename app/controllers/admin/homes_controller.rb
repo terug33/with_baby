7bb7f799ea70aaaad65285691853051e5b84db96
EC2 Default User <ec2-user@ip-172-31-33-146.ap-northeast-1.compute.internal>
2025-08-17 08:17:59 +0000

    レスポンシブ対応を進めました

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,4 +1,7 @@
 class Admin::HomesController < Admin::BaseController
+
+  skip_before_action :authenticate_admin!, only: [:top]
+
   def top
     @stores = Store.order(created_at: :desc).limit(3)
     @categories = Category.all.order(:name)
